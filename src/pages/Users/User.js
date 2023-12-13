53c53d33564a937f319ce53ec3bc3c809f569dfb
Emboiii <153010066+Emboiii@users.noreply.github.com>
2023-12-13 07:36:49 +0700

    Update User.js

--- src/pages/Users/User.js
+++ src/pages/Users/User.js
@@ -5,19 +5,75 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 
 function Users() {
-    return (
-        <div className="container mt-5">
-            <div className="card p-5 shadow">
-                <div className="Title">
-                    <h1>Data Users</h1>
-                </div>
-                <div className="conten">
-                    <button className="btn btn-primary mt-4">
-                        Tambah Data
-                    </button>
-                </div>
-            </div>
+  // Hook useState untuk menyimpan data pengguna dan useEffect untuk memanggil fungsi fectData saat komponen dimuat
+  const [listuser, setListuser] = useState([]);
+
+  useEffect(() => {
+    fectData();
+  }, []);
+
+  // fectData adalah fungsi asinkron yang menggunakan axios untuk mengambil data pengguna dari backend dan mengatur state listuser dengan data tersebut.
+  const fectData = async () => {
+    const response = await axios.get("http://localhost:5154/User/GetAllData");
+    const data = await response.data.data;
+    setListuser(data);
+    console.log(data);
+  };
+
+  const kolom = [
+    {
+      name: "ID",
+      selector: row => row.id,
+      sortable: true,
+    },
+    {
+      name: "Nama",
+      selector: row => row.nama,
+      sortable: true,
+    },
+    {
+      name: "Username",
+      selector: row => row.username,
+      sortable: true,
+    },
+    {
+      name: "Status",
+      selector: row => row.status,
+      sortable: true,
+    },
+    {
+      name: "Role",
+      selector: row => row.role,
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      selector: row => (
+        <div style={{ display: "flex", gap: "10px" }}>
+          <Link to={"/users/edit/" + row.id} className="btn btn-warning">
+            Edit
+          </Link>
+          <Link to={"/users/delete/" + row.id} className="btn btn-danger">
+            Delete
+          </Link>
         </div>
-    );
+      ),
+      width: "170px",
+    },
+  ];
+
+  return (
+    <div className="container mt-5">
+      <div className="card p-5 shadow">
+        <div className="Title">Data Users</div>
+        <div className="conten">
+          <Link to="/users/add" className="btn btn-primary mt-4">
+            Tambah Data
+          </Link>
+          <DataTable columns={kolom} data={listuser} pagination />
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Users;
