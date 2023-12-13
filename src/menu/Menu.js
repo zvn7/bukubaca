bc13fbb0a023a3ebbffa2fce9d8879fce0c4e350
zvn7 <aammaul7@gmail.com>
2023-12-13 15:34:12 +0700

    Update Menu

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,54 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css"; // Include Bootstrap CSS
 
 const Menu = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <Navbar bg="primary" expand="lg" variant="dark">
             <div className="container">
                 <Link to="/" className="navbar-brand">
                     Buku Baca
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div
-                    className="collapse navbar-collapse justify-content-end"
-                    id="navbarNav"
-                >
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/buku" className="nav-link">
-                                Buku
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/histori" className="nav-link">
+                <Navbar.Toggle aria-controls="navbarNav" />
+                <Navbar.Collapse id="navbarNav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        {/* <Nav.Link as={Link} to="/buku">
+                            Buku
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/histori">
+                            Histori Peminjaman
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/users">
+                            User
+                        </Nav.Link> */}
+                        <NavDropdown title="Public" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/buku-view">
+                                Buku View
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Anggota" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/buku-view">
+                                Buku View
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/histori">
                                 Histori Peminjaman
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/users" className="nav-link">
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Petugas" id="basic-nav-dropdown">
+                            <NavDropdown.Item as={Link} to="/buku">
+                                Buku
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/histori">
+                                Histori Peminjaman
+                            </NavDropdown.Item>
+                            <NavDropdown.Item as={Link} to="/users">
                                 User
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </div>
-        </nav>
+        </Navbar>
     );
 };
 
